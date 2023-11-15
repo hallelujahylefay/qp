9290a91d8d091ea4d34344ce5fd809d9aabb99bc
ylf <iivodka5@gmail.com>
2023-11-16 00:08:29 +0200

    plot

--- qp.py
+++ qp.py
@@ -3,7 +3,8 @@ import jax.numpy as jnp
 
 jax.config.update('jax_enable_x64', True)
 max_iter = 100
-max_iter_centering = 10 # ~ ceil(log(m eps) / log(mu)) ~= 10
+max_iter_centering = 10  # ~ ceil(log(m eps) / log(mu)) ~= 10
+
 
 @jax.jit
 def qp(v, Q, p):
@@ -34,12 +35,12 @@ def centering_step(Q, p, A, b, t, v0, eps, max_iter=max_iter_centering):
 
     def iter_newton(inps):
         n_iter, _, v, vs, t = inps
-        vs = vs.at[n_iter].set(v)
         jac, hessian = jax.grad(centering_objective, argnums=1)(t, v), jax.hessian(centering_objective, argnums=1)(t, v)
         descent_step = - jnp.linalg.solve(hessian, jac)
         lambda_square = - jac.T @ descent_step
         _, t_star = backtracking_line_search(v, descent_step, lambda_square)
         v += t_star * descent_step
+        vs = vs.at[n_iter].set(v)
         return n_iter + 1, lambda_square, v, vs, t
 
     beta = 0.5
@@ -54,15 +55,15 @@ def centering_step(Q, p, A, b, t, v0, eps, max_iter=max_iter_centering):
                     )) & (n_iter < max_iter)
 
         def iter_backtracking(inps):
-            n_iter, t_p = inps
+            n_iter_bt, t_p = inps
             t_p *= beta
-            return n_iter + 1, t_p
+            return n_iter_bt + 1, t_p
 
-        t_star = jax.lax.while_loop(cond, iter_backtracking, (0, 1.0))
-        return t_star
+        bt = jax.lax.while_loop(cond, iter_backtracking, (0, 1.0))
+        return bt
 
     vs0 = jnp.zeros((max_iter, *v0.shape))
-    n_iter, _, v, vs, t = jax.lax.while_loop(cond, iter_newton, (1, 3 * eps, v0, vs0, t))
+    n_iter, _, v, vs, t = jax.lax.while_loop(cond, iter_newton, (0, 3 * eps, v0, vs0, t))
     return n_iter, v, vs
 
 
@@ -77,7 +78,8 @@ def barr_method(Q, p, A, b, v0, eps, mu=10, max_iter=max_iter):
 
     def iter_barrier(inps):
         n_iter, n_iters_centering, v, vs, t = inps
-        n_iter_centering, v_star, vs_centering = centering_step(Q, p, A, b, t, v, eps) # n_iter_centering must be hardcoded
+        n_iter_centering, v_star, vs_centering = centering_step(Q, p, A, b, t, v,
+                                                                eps)  # n_iter_centering must be hardcoded
         n_iters_centering = n_iters_centering.at[n_iter].set(n_iter_centering)
         vs = vs.at[n_iter].set(vs_centering)
         t *= mu
@@ -90,5 +92,6 @@ def barr_method(Q, p, A, b, v0, eps, mu=10, max_iter=max_iter):
     vs0 = jnp.zeros((max_iter, max_iter_centering, *v0.shape))
     vs0 = vs0.at[0].set(jnp.full((max_iter_centering, *v0.shape), v0))
     n_iter, n_iters_centering, v_optimal, vs, _ = jax.lax.while_loop(cond, iter_barrier,
-                                                                     (1, jnp.zeros((max_iter_centering,), dtype=int), v0, vs0, 1))
+                                                                     (1, jnp.zeros((max_iter, ),
+                                                                                  dtype=int), v0, vs0, 1))
     return n_iter, n_iters_centering, v_optimal, vs
--- test.py
+++ test.py
@@ -1,35 +1,40 @@
 import jax
 import jax.numpy as jnp
-from lasso import solve_lasso, lasso_objective
+from lasso import solve_lasso
 from timeit import timeit
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+import numpy as np
 
-N = 100  # number of lasso problems
+
+
+N = 5  # number of lasso problems
 OP_key = jax.random.PRNGKey(0)
 keys = jax.random.split(OP_key, N)
 
-shape = (40, 10)
-mus = jnp.array([2, 10, 15, 50, 100])
+shape = (2, 6)
+mus = jnp.array([2, 10, 15, 50, 75, 100, 150, 200])
 penalization = 10
 distrib = jax.random.uniform
 
-for mu in mus:
+wnorm = np.zeros((len(mus), N))
+las_obj = np.zeros((len(mus), N))
+
+for idx, mu in enumerate(mus):
     @timeit
     @jax.vmap
     def wrapper_lasso_barr_method(key):
         X, y = distrib(key, shape=shape), distrib(key, shape=(shape[0],))
         return solve_lasso(X, y, penalization=penalization, mu=mu, eps=1e-6)
 
-
-    n_iters, n_iters_centering, _, _, values_for_dual_objective, lasso_objective_values = wrapper_lasso_barr_method(
+    n_iters, n_iters_centering, _, primals, values_for_dual_objective, lasso_objective_values = wrapper_lasso_barr_method(
         keys)
+
     for i in range(N):
         iteration = n_iters[i]
-        n_iters_centering[i]
         reconstructed_values_for_dual_objective = jnp.concatenate(
-            [jnp.trim_zeros(values_for_dual_objective[i][:iteration][j][1:n_iters_centering[i][j]]) for j in
+            [(values_for_dual_objective[i][:iteration][j][:n_iters_centering[i][j]]) for j in
              range(iteration)])
+
         min = jnp.min(reconstructed_values_for_dual_objective)
         plt.semilogy(range(len(reconstructed_values_for_dual_objective)), reconstructed_values_for_dual_objective - min)
 
@@ -37,28 +42,42 @@ for mu in mus:
     plt.savefig(f"lasso_qp_{mu}", dpi=500)
     plt.close()
 
-import numpy as np
+    for i in range(N):
+        iteration = n_iters[i]
+        reconstructed_values_for_primal = jnp.linalg.norm(jnp.concatenate(
+            [(primals[i][:iteration][j][:n_iters_centering[i][j]]) for j in
+             range(1, iteration)]),
+            axis=-1)
+        wnorm[idx][i] = reconstructed_values_for_primal[-1]
+        plt.semilogy(range(len(reconstructed_values_for_primal)), reconstructed_values_for_primal)
 
-shape = (40, 10)
-X = np.zeros(shape)
-y = np.zeros(shape[0])
-X[:shape[0] // 2, 0] = 0
-X[shape[0] // 2:, 0] = 2.5
-y[:shape[0] // 2] = 0.5
-y[shape[0] // 2:] = 1
-X = jnp.array(X)
-y = jnp.array(y)
+    plt.title(f"{N} lasso problems w/ shape {shape[0], shape[1]}, norm w, mu={mu}")
+    plt.savefig(f"lasso_qp_norm_{mu}", dpi=500)
+    plt.close()
 
-lasso_minimize = minimize(lasso_objective, jnp.zeros(shape[-1]), args=(X, y, penalization), method='L-BFGS-B')
-n_iter, _, _, _, _, objective = solve_lasso(X, y, penalization=penalization, mu=2)
-print(lasso_minimize)
-print(objective[:n_iter])
-X = jnp.array([[1, 2], [3, 4]])
-y = jnp.array([1, 2])
-X = jnp.ones(shape)
-y = jnp.ones(shape[0])
 
-lasso_minimize = minimize(lasso_objective, jnp.zeros(shape[-1]), args=(X, y, penalization), method='L-BFGS-B')
-n_iter, _, _, _, _, objective = solve_lasso(X, y, penalization=penalization, mu=2)
-print(lasso_minimize)
-print(objective[:n_iter])
+    for i in range(N):
+        iteration = n_iters[i]
+        reconstructed_values_for_lasso_objective_values = jnp.concatenate(
+            [(lasso_objective_values[i][:iteration][j][:n_iters_centering[i][j]]) for j in
+             range(iteration)])
+        las_obj[idx][i] = reconstructed_values_for_lasso_objective_values[-1]
+        plt.semilogy(range(len(reconstructed_values_for_lasso_objective_values)),
+                     reconstructed_values_for_lasso_objective_values)
+
+    plt.title(f"{N} lasso problems w/ shape {shape[0], shape[1]}, lasso objective, mu={mu}")
+    plt.savefig(f"lasso_qp_lasso_objective_{mu}", dpi=500)
+    plt.close()
+
+for i in range(N):
+    plt.plot(mus, wnorm[:,i])
+plt.title(f"{N} lass problems w/ shape {shape[0], shape[1]}, w norm w.r.t mu")
+plt.savefig(f"lass_qp_norm_mus", dpi=500)
+plt.close()
+
+
+for i in range(N):
+    plt.plot(mus, las_obj[:,i])
+plt.title(f"{N} lass problems w/ shape {shape[0], shape[1]}, lasso obj. w.r.t mu")
+plt.savefig(f"lass_qp_obj_mus", dpi=500)
+plt.close()
