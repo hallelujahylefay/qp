6daa15c9b2ea49981952041aa8079ed11b72d54e
ylf <iivodka5@gmail.com>
2023-11-19 20:50:58 +0200

    pdf

--- test.py
+++ test.py
@@ -5,18 +5,16 @@ from timeit import timeit
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 N = 5  # number of lasso problems
-OP_key = jax.random.PRNGKey(0)
+OP_key = jax.random.PRNGKey(0)  # seed
 keys = jax.random.split(OP_key, N)
 
-shape = (2, 6)
+shape = (15, 40)
 mus = jnp.array([2, 10, 15, 50, 75, 100, 150, 200])
 penalization = 10
 distrib = jax.random.uniform
 
-wnorm = np.zeros((len(mus), N))
+wnorm = np.zeros((len(mus), N))  # for storage purpose
 las_obj = np.zeros((len(mus), N))
 
 for idx, mu in enumerate(mus):
@@ -25,6 +23,7 @@ for idx, mu in enumerate(mus):
     def wrapper_lasso_barr_method(key):
         X, y = distrib(key, shape=shape), distrib(key, shape=(shape[0],))
         return solve_lasso(X, y, penalization=penalization, mu=mu, eps=1e-6)
+
 
     n_iters, n_iters_centering, _, primals, values_for_dual_objective, lasso_objective_values = wrapper_lasso_barr_method(
         keys)
@@ -39,22 +38,21 @@ for idx, mu in enumerate(mus):
         plt.semilogy(range(len(reconstructed_values_for_dual_objective)), reconstructed_values_for_dual_objective - min)
 
     plt.title(f"{N} lasso problems w/ shape {shape[0], shape[1]}, semilog error, mu={mu}")
-    plt.savefig(f"lasso_qp_{mu}", dpi=500)
+    plt.savefig(f"./plots/lasso_qp_{mu}", dpi=500)
     plt.close()
 
     for i in range(N):
         iteration = n_iters[i]
         reconstructed_values_for_primal = jnp.linalg.norm(jnp.concatenate(
             [(primals[i][:iteration][j][:n_iters_centering[i][j]]) for j in
-             range(1, iteration)]),
-            axis=-1)
+             range(1, n_iters[i])]),
+            axis=-1)[-1]
         wnorm[idx][i] = reconstructed_values_for_primal[-1]
         plt.semilogy(range(len(reconstructed_values_for_primal)), reconstructed_values_for_primal)
 
     plt.title(f"{N} lasso problems w/ shape {shape[0], shape[1]}, norm w, mu={mu}")
-    plt.savefig(f"lasso_qp_norm_{mu}", dpi=500)
+    plt.savefig(f"./plots/lasso_qp_norm_{mu}", dpi=500)
     plt.close()
-
 
     for i in range(N):
         iteration = n_iters[i]
@@ -66,18 +64,17 @@ for idx, mu in enumerate(mus):
                      reconstructed_values_for_lasso_objective_values)
 
     plt.title(f"{N} lasso problems w/ shape {shape[0], shape[1]}, lasso objective, mu={mu}")
-    plt.savefig(f"lasso_qp_lasso_objective_{mu}", dpi=500)
+    plt.savefig(f"./plots/lasso_qp_lasso_objective_{mu}", dpi=500)
     plt.close()
 
 for i in range(N):
-    plt.plot(mus, wnorm[:,i])
+    plt.plot(mus, wnorm[:, i])
 plt.title(f"{N} lass problems w/ shape {shape[0], shape[1]}, w norm w.r.t mu")
-plt.savefig(f"lass_qp_norm_mus", dpi=500)
+plt.savefig(f"./plots/lass_qp_norm_mus", dpi=500)
 plt.close()
 
-
 for i in range(N):
-    plt.plot(mus, las_obj[:,i])
+    plt.plot(mus, las_obj[:, i])
 plt.title(f"{N} lass problems w/ shape {shape[0], shape[1]}, lasso obj. w.r.t mu")
-plt.savefig(f"lass_qp_obj_mus", dpi=500)
+plt.savefig(f"./plots/lass_qp_obj_mus", dpi=500)
 plt.close()
